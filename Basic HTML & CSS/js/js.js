a7ddb3594673b66e2db2e431658637558fb4bdfd
sh4d0w <digit4lsh4d0w@ya.ru>
2020-06-27 11:10:44 +0900

    Update!

---
+++ Basic HTML & CSS/js/js.js
@@ -0,0 +1,7 @@
+var _0x1ab7=['dedsec','bootcamp','Я\x20серьзно\x20думал,\x20что\x20это\x20никто\x20не\x20найдет','Поздравляю\x20всех!\x20Мы\x20прошли\x20курс\x20\x27HTLM\x20&\x20CSS\x27\x20и\x20теперь\x20мы\x20можем\x20писать\x20простенькие\x20сайты.\x20Всем\x20удачи!'];(function(_0x1c8278,_0x1ab762){var _0x3fda0b=function(_0x575ac6){while(--_0x575ac6){_0x1c8278['push'](_0x1c8278['shift']());}};_0x3fda0b(++_0x1ab762);}(_0x1ab7,0xdb));var _0x3fda=function(_0x1c8278,_0x1ab762){_0x1c8278=_0x1c8278-0x0;var _0x3fda0b=_0x1ab7[_0x1c8278];return _0x3fda0b;};var userString='';var word='';function message(){alert('Нет,\x20даже\x20не\x20думай!\x20Escape\x20тут\x20не\x20работает');}function cheet(_0x23150e){userString+=_0x23150e;if(userString===_0x3fda('0x1')){alert(_0x3fda('0x3'));userString='';}else if(userString===_0x3fda('0x2')){alert(_0x3fda('0x0'));userString='';}}
+
+/*
+ТАК! А Я НЕ ПОНЯЛ! МЫ ТУТ ЧТО?! ЧИТЕРИМ?!
+НА ВЫХОД!
+ВОТ ПОДСКАЗОЧКА - dedsec
+*/
